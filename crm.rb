1d9de93e7b4e0dc4f9db6450af8a464214086dc5
Nancy Wood <nancywood13@gmail.com>
2015-06-16 17:07:36 -0400

    unfake data

--- crm.rb
+++ crm.rb
@@ -2,17 +2,19 @@ require_relative 'rolodex'
 require_relative 'contact'
 require 'sinatra'
 
+$rolodex= Rolodex.new 
+
 get '/' do 
 	@crm_app_name = "Nancy Wood's CRM"
 	erb :index  
 end
 
 get '/contacts' do
-	@contacts = []
-	@contacts << Contact.new("Mike", "Fich", "mike@fich.com", "cool guy")
-	@contacts << Contact.new("Rhea", "Really", "rhea@really.com", "cool gal")
-	@contacts << Contact.new("Keegan", "Dude", "keegan@dude.com", "the dude")
-	@contacts << Contact.new("Nancy", "Wood", "nancy@librarian.com", "the librarian")
+	# @contacts = []
+	# @contacts << Contact.new("Mike", "Fich", "mike@fich.com", "cool guy")
+	# @contacts << Contact.new("Rhea", "Really", "rhea@really.com", "cool gal")
+	# @contacts << Contact.new("Keegan", "Dude", "keegan@dude.com", "the dude")
+	# @contacts << Contact.new("Nancy", "Wood", "nancy@librarian.com", "the librarian")
 
 	erb :contacts
 end
