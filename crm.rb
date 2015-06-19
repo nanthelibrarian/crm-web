b480ae4d7ed101d3c81f08c5d482d2858833d0aa
Nancy Wood <nancywood13@gmail.com>
2015-06-19 14:36:10 -0400

    show single contact create route for finding contacts

--- crm.rb
+++ crm.rb
@@ -3,7 +3,18 @@ require_relative 'contact'
 require 'sinatra'
 require 'sinatra/reloader'
 
+# DataMapper.setup(:default, 'sqlite:crm_development.sqlite')
+
+# Class Contact
+# include DataMapper::Resource
+# end
+
+
 $rolodex= Rolodex.new 
+
+$rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
+
+
 
 get '/' do 
 	@crm_app_name = "Nancy Wood's CRM"
@@ -25,7 +36,7 @@ get '/contacts' do
 	# 			-start rendering contacts.erb
 	# 	-finish rendering layout.erb
 	# - send it to the user
-	
+
 
 end
 
@@ -33,8 +44,14 @@ get '/contacts/new' do
 	erb :new_contact
 end
 
+get "/contacts/1000" do
+  @contact = $rolodex.find(1000)
+  erb :show_contact
+end
+
 post '/contacts' do
   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
   $rolodex.add_contact(new_contact)
   redirect to('/contacts')
 end
+
